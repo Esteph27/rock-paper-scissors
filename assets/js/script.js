1cf5db0cca262329c0bfc1148faf43627458862b
Esteph27 <esteph.csa@gmail.com>
2021-09-17 20:15:32 +0000

    add click event to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// add click event to buttons 
+
+$('#btn-r').click(function() {selectWeapon()}) 
+$('#btn-p').click(function() {selectWeapon()})
+$('#btn-s').click(function() {selectWeapon()}) 
+
+function selectWeapon() {
+    console.log('weapon selected!')
+}
